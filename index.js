b34e70840dd4d9b0cb58035798d0e0f7f0593494
Kevin Omondi <Kevin.Omondi@outlook.com>
2022-05-16 12:13:16 +0300

    add index.js and add images to images folder

---
+++ index.js
@@ -0,0 +1,6 @@
+function upperCase() {
+        const x = document.getElementById("fname");
+        x.value = x.value.toUpperCase();
+        const y = document.getElementById("lname");
+        y.value = y.value.toUpperCase();
+        }
